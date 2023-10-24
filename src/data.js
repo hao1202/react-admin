ad279b5ba09f1c046a67537f0b1a46db7b320ff2
hao1202 <tranvanhao2122003@gmail.com>
2023-10-24 18:51:38 +0700

    design menu

---
+++ src/data.js
@@ -0,0 +1,116 @@
+export const menu = [
+  {
+    id: 1,
+    title: "main",
+    listItems: [
+      {
+        id: 1,
+        title: "Homepage",
+        url: "/",
+        icon: "assets/home.svg",
+      },
+      {
+        id: 2,
+        title: "Profile",
+        url: "/users/1",
+        icon: "assets/user.svg",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "lists",
+    listItems: [
+      {
+        id: 1,
+        title: "Users",
+        url: "/users",
+        icon: "assets/user.svg",
+      },
+      {
+        id: 2,
+        title: "Products",
+        url: "/products",
+        icon: "assets/product.svg",
+      },
+      {
+        id: 3,
+        title: "Orders",
+        url: "/orders",
+        icon: "assets/order.svg",
+      },
+      {
+        id: 4,
+        title: "Posts",
+        url: "/posts",
+        icon: "assets/post2.svg",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "general",
+    listItems: [
+      {
+        id: 1,
+        title: "Elements",
+        url: "/",
+        icon: "assets/element.svg",
+      },
+      {
+        id: 2,
+        title: "Notes",
+        url: "/",
+        icon: "assets/note.svg",
+      },
+      {
+        id: 3,
+        title: "Forms",
+        url: "/",
+        icon: "assets/form.svg",
+      },
+      {
+        id: 4,
+        title: "Calendar",
+        url: "/",
+        icon: "assets/calendar.svg",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Maintenance",
+    listItems: [
+      {
+        id: 1,
+        title: "Settings",
+        url: "/",
+        icon: "assets/setting.svg",
+      },
+      {
+        id: 2,
+        title: "Backups",
+        url: "/",
+        icon: "assets/backup.svg",
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "analytics",
+    listItems: [
+      {
+        id: 1,
+        title: "Charts",
+        url: "/",
+        icon: "assets/chart.svg",
+      },
+      {
+        id: 2,
+        title: "Logs",
+        url: "/",
+        icon: "assets/log.svg",
+      },
+    ],
+  },
+];
